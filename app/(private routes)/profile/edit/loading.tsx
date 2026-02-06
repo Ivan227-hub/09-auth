0d147befc532fdd438a97cc1ceb8a4a3bffb3f1a
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-02-06 13:52:52 +0200

    09

---
+++ app/(private routes)/profile/edit/loading.tsx
@@ -0,0 +1,10 @@
+// app/(private routes)/profile/edit/loading.tsx
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <p>Завантаження...</p>
+    </div>
+  );
+}
