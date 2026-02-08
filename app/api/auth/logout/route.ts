2989abbe84f57bac01d948e111b2db8be6ee378b
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2026-02-08 21:57:50 +0200

    9

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,9 +1,9 @@
-// /app/api/auth/logout/route.ts
 import { NextResponse } from 'next/server';
 import { api } from '../../../api';
 import { cookies } from 'next/headers';
 import { isAxiosError } from 'axios';
-import { logErrorResponse } from '../../../_utils/utils'; // исправленный путь
+import { logErrorResponse } from '@utils/utils';
+
 
 export async function POST() {
   try {
@@ -12,23 +12,16 @@ export async function POST() {
     const accessToken = cookieStore.get('accessToken')?.value;
     const refreshToken = cookieStore.get('refreshToken')?.value;
 
-    await api.post(
-      'auth/logout',
-      null,
-      {
-        headers: {
-          Cookie: `accessToken=${accessToken}; refreshToken=${refreshToken}`,
-        },
-      }
-    );
+    await api.post('auth/logout', null, {
+      headers: {
+        Cookie: `accessToken=${accessToken}; refreshToken=${refreshToken}`,
+      },
+    });
 
     cookieStore.delete('accessToken');
     cookieStore.delete('refreshToken');
 
-    return NextResponse.json(
-      { message: 'Logged out successfully' },
-      { status: 200 }
-    );
+    return NextResponse.json({ message: 'Logged out successfully' }, { status: 200 });
   } catch (error) {
     if (isAxiosError(error)) {
       logErrorResponse(error.response?.data);
@@ -39,9 +32,6 @@ export async function POST() {
     }
 
     logErrorResponse({ message: (error as Error).message });
-    return NextResponse.json(
-      { error: 'Internal Server Error' },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
 }
